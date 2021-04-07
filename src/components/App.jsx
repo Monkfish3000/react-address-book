a807248a4ac0d5e50c8638a75607fb8d42c0227e
Michael Sutcliffe <michaelsutcliffe86@gmail.com>
2021-04-07 15:53:29 +0200

    address book react practice

---
+++ src/components/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function App() {
+  return (
+    <div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={contacts[0].name}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+        img={contacts[0].imgURL}
+      />
+      <Card
+        name={contacts[1].name}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+        img={contacts[1].imgURL}
+      />
+
+      <Card
+        name={contacts[2].name}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+        img={contacts[2].imgURL}
+      />
+
+      <Card
+        name={contacts[3].name}
+        tel={contacts[3].phone}
+        email={contacts[3].email}
+        img={contacts[3].imgURL}
+      />
+    </div>
+  );
+}
+
+export default App;
